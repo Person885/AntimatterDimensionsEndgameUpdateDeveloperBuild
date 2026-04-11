9e0734208c85bb72a379a88013791d393f65e493
Supersonic Seven <b.rockholz@icloud.com>
2026-04-11 14:07:24 -0400

    Add omniGlyphs generation in effarig.js
    Add omniGlyphs to inventory for each active slot.

--- src/core/celestials/effarig.js
+++ src/core/celestials/effarig.js
@@ -26,6 +26,9 @@ export const Effarig = {
     player.celestials.effarig.run = true;
     recalculateAllGlyphs();
     Tab.reality.glyphs.show(false);
+    for (x = 0; x < Glyphs.activeSlotCount; x++) {
+      for (const type of BASIC_GLYPH_TYPES) Glyphs.addToInventory(GlyphGenerator.omniGlyph(type));
+    }
   },
   get isRunning() {
     return player.celestials.effarig.run;

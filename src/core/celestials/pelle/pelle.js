52e70f41df23054e23696e615c3b5116e63f1fce
Supersonic Seven <b.rockholz@icloud.com>
2026-04-13 09:17:43 -0400

    Update condition for first divine milestone check
    Added a condition to check if player.disablePostReality is false when checking if the first divine milestone is reached.

--- src/core/celestials/pelle/pelle.js
+++ src/core/celestials/pelle/pelle.js
@@ -127,7 +127,7 @@ export const Pelle = {
       AutomatorBackend.stop();
     }
 
-    if (DivinityMilestone.firstDivine.isReached) {
+    if (DivinityMilestone.firstDivine.isReached && !player.disablePostReality) {
       player.celestials.pelle.upgrades = new Set([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22]);
       player.celestials.pelle.rebuyables.antimatterDimensionMult = 47;
       player.celestials.pelle.rebuyables.timeSpeedMult = 35;

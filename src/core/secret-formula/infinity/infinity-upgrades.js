b4a9f595ee783d3b8b6f017f5409c5be48b0d7a5
Supersonic Seven <b.rockholz@icloud.com>
2026-04-16 20:11:37 -0400

    Update costIncreaseThreshold calculation for ipMult

--- src/core/secret-formula/infinity/infinity-upgrades.js
+++ src/core/secret-formula/infinity/infinity-upgrades.js
@@ -229,7 +229,7 @@ export const infinityUpgrades = {
     cost: () => InfinityUpgrade.ipMult.cost,
     checkRequirement: () => Achievement(41).isUnlocked,
     costCap: () => Alpha.isRunning ? Decimal.pow10(AlphaUnlocks.infinityChallenges.effects.nerf.effectOrDefault(Decimal.log10((BreakEternityUpgrade.doubleIPUncap.isBought && !player.disablePostReality) ? DC.BEMAX : DC.E6E6).sub(1))).times(10) : ((BreakEternityUpgrade.doubleIPUncap.isBought && !player.disablePostReality) ? DC.BEMAX : DC.E6E6),
-    costIncreaseThreshold: () => (EndgameUpgrade(21).isBought && !player.disablePostReality) ? DC.BEMAX : DC.E3E6,
+    costIncreaseThreshold: () => (EndgameUpgrade(21).isBought && !player.disablePostReality) ? Decimal.pow10(1e125) : DC.E3E6,
     description: () => `Multiply Infinity Points from all sources by ${formatX(2)}`,
     // Normally the multiplier caps at e993k or so with 3300000 purchases, but if the cost is capped then we just give
     // an extra e7k to make the multiplier look nice

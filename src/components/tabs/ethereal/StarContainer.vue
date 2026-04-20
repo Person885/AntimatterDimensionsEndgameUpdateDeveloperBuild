a9393ee087b129e7b5d6c85a3773a0f7863a4452
Supersonic Seven <b.rockholz@icloud.com>
2026-04-20 13:29:58 -0400

    Rename StarBox to StarContainer and refactor code
    Renamed component from StarBox to StarContainer and updated related properties and methods. Adjusted template conditions and class bindings.

---
+++ src/components/tabs/ethereal/StarContainer.vue
@@ -0,0 +1,60 @@
+<script>
+export default {
+  name: "StarContainer",
+  props: {
+    getStar: {
+      type: Function,
+      required: true
+    }
+  },
+  data() {
+    return {
+      isUnlocked: false,
+    };
+  },
+  computed: {
+    star() {
+      return this.getStar();
+    },
+    reward() {
+      return this.star.reward;
+    },
+    description() {
+      return this.star.config.description(this.reward);
+    },
+    rewardClassObject() {
+      return {
+        "o-star__container": true,
+        [`o-star__container--${star.config.name}`]: true,
+      };
+    },
+  },
+  methods: {
+    update() {
+      this.isUnlocked = this.star.isUnlocked;
+    },
+    starReset() {
+      resetForStar(this.star.id);
+    }
+  }
+};
+</script>
+
+<template>
+  <div
+    v-if="config.isUnlocked"
+    class="l-star-container"
+  >
+    <button
+      :class="rewardClassObject"
+    >
+      <span>
+        {{ reward }}
+      </span>
+    </button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>

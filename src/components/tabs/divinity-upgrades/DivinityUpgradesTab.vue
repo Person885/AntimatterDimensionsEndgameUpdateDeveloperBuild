1ce84cf67fcff6d90d288c68445ec6bdb33a3fde
Supersonic Seven <b.rockholz@icloud.com>
2026-04-28 15:40:09 -0400

    Add DivinityUpgradesTab component with upgrades grid

---
+++ src/components/tabs/divinity-upgrades/DivinityUpgradesTab.vue
@@ -0,0 +1,38 @@
+<script>
+import DivinityUpgradeButton from "./DivinityUpgradeButton";
+
+export default {
+  name: "DivinityUpgradesTab",
+  components: {
+    DivinityUpgradeButton
+  },
+  computed: {
+    upgrades: () => DivinityUpgrade.all
+  },
+  methods: {
+    id(row, column) {
+      return (row - 1) * 5 + column - 1;
+    }
+  }
+};
+</script>
+
+<template>
+  <div class="l-divinity-upgrade-grid">
+    <div
+      v-for="row in 5"
+      :key="row"
+      class="l-divinity-upgrade-grid__row"
+    >
+      <DivinityUpgradeButton
+        v-for="column in 5"
+        :key="id(row, column)"
+        :upgrade="upgrades[id(row, column)]"
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>

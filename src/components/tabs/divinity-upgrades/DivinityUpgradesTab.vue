8fa34355d36c971ac9a6d79cab2b84811de15dc7
Supersonic Seven <b.rockholz@icloud.com>
2026-04-28 17:12:32 -0400

    Refactor DivinityUpgradesTab to use grid method

--- src/components/tabs/divinity-upgrades/DivinityUpgradesTab.vue
+++ src/components/tabs/divinity-upgrades/DivinityUpgradesTab.vue
@@ -7,27 +7,39 @@ export default {
     DivinityUpgradeButton
   },
   computed: {
-    upgrades: () => DivinityUpgrades.all
-  },
-  methods: {
-    id(row, column) {
-      return (row - 1) * 5 + column - 1;
+    grid() {
+      return [
+        [
+          DivinityUpgrade.divineL1U1,
+          DivinityUpgrade.divineL1U2,
+          DivinityUpgrade.divineL1U3,
+          DivinityUpgrade.divineL1U4,
+          DivinityUpgrade.divineL1U5,
+        ],
+        [
+          DivinityUpgrade.divineL1U6,
+          DivinityUpgrade.divineL1U7,
+          DivinityUpgrade.divineL1U8,
+          DivinityUpgrade.divineL1U9,
+          DivinityUpgrade.divineL1U10,
+        ]
+      ];
     }
-  }
+  },
 };
 </script>
 
 <template>
   <div class="l-divinity-upgrade-grid">
     <div
-      v-for="row in 5"
-      :key="row"
+      v-for="(column, columnId) in grid"
+      :key="columnId"
       class="l-divinity-upgrade-grid__row"
     >
       <DivinityUpgradeButton
-        v-for="column in 5"
-        :key="id(row, column)"
-        :upgrade="upgrades[id(row, column)]"
+        v-for="upgrade in column"
+        :key="upgrade.id"
+        :upgrade="upgrade"
       />
     </div>
   </div>

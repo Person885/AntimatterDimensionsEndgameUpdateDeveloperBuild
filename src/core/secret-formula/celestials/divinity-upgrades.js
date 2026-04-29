cdd61b90f1b4c4d2ea750f156bc516966e9aea22
Supersonic Seven <b.rockholz@icloud.com>
2026-04-28 20:06:22 -0400

    Adjust effect calculation for divineL1U3 upgrade

--- src/core/secret-formula/celestials/divinity-upgrades.js
+++ src/core/secret-formula/celestials/divinity-upgrades.js
@@ -22,7 +22,7 @@ export const divinityUpgrades = {
     layer: 1,
     cost: new Decimal(1e20),
     description: "Divine Dimensions are boosted based on Celestial Points",
-    effect: () => Decimal.log10(player.endgame.celestialPoints).div(Decimal.log10(DC.NUMMAX)).max(1),
+    effect: () => Decimal.log10(player.endgame.celestialPoints).div(100).max(1),
     formatEffect: value => formatX(value, 2, 2)
   },
   divineL1U4: {

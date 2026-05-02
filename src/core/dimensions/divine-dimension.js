b7835292401d74651ad8441c00b947a9bc3f7add
Person885 <keyboaqwerty@gmail.com>
2026-05-02 11:03:53 -0600

    Update divineDimensionCommonMultiplier and HARDCAP calculations

--- src/core/dimensions/divine-dimension.js
+++ src/core/dimensions/divine-dimension.js
@@ -3,7 +3,7 @@ import { DimensionState } from "./dimension";
 export function divineDimensionCommonMultiplier() {
   let mult = DC.D1;
   mult = mult.timesEffectsOf(DivinityUpgrade.divineL1U3, DivinityUpgrade.divineL1U6).times(
-    DivinityMilestone.hadronEmpowerment.isReached ? 77 : 1);
+    DivinityMilestone.hadronEmpowerment.isReached ? 77 : 1).times(Decimal.pow(7, Decimal.log10(player.celestials.pelle.divinity.divineStars.add(1)).add(1));
   return mult;
 }
 
@@ -148,7 +148,7 @@ export const DivineDimensions = {
   all: DivineDimension.index.compact(),
 
   get HARDCAP() {
-    return DC.NUMMAX.times(Decimal.pow(1.1, 0));
+    return DC.NUMMAX.pow(Decimal.log10(player.celestials.pelle.divinity.divineStars.add(1)).add(1));
   },
 
   get energyPerSecond() {
